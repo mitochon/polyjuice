b691fc46e0793c05fa37a27c711f8befe22f3ca4
Francisco Tanudjaja <fcstanud@gmail.com>
2018-02-23 07:05:24 -0800

    variant coord

--- src/main/scala/polyjuice/model/alleleCoord.scala
+++ src/main/scala/polyjuice/model/alleleCoord.scala
@@ -5,20 +5,24 @@ sealed trait AlleleCoord {
   def pos: Int
 }
 
-case class Snv(contig: String, pos: Int, ref: Base, alt: Base) extends AlleleCoord
+sealed trait VariantCoord extends AlleleCoord
 
-case class Ins(contig: String, pos: Int, ref: Option[Base], alt: Seq[Base]) extends AlleleCoord {
+case class Single(contig: String, pos: Int, base: Base) extends AlleleCoord
+
+case class Snv(contig: String, pos: Int, ref: Base, alt: Base) extends VariantCoord
+
+case class Ins(contig: String, pos: Int, ref: Option[Base], alt: Seq[Base]) extends VariantCoord {
   require(alt.nonEmpty && ref.exists(_ == alt(0)))
 }
 
-case class Del(contig: String, pos: Int, ref: Seq[Base], alt: Option[Base]) extends AlleleCoord {
+case class Del(contig: String, pos: Int, ref: Seq[Base], alt: Option[Base]) extends VariantCoord {
   require(ref.nonEmpty && alt.exists(_ == ref(0)))
 }
 
-case class Mnp(contig: String, pos: Int, ref: Seq[Base], alt: Seq[Base]) extends AlleleCoord {
+case class Mnp(contig: String, pos: Int, ref: Seq[Base], alt: Seq[Base]) extends VariantCoord {
   require(ref.nonEmpty && alt.nonEmpty && ref.length == alt.length)
 }
 
-case class Clumped(contig: String, pos: Int, ref: Seq[Base], alt: Seq[Base]) extends AlleleCoord {
+case class Clumped(contig: String, pos: Int, ref: Seq[Base], alt: Seq[Base]) extends VariantCoord {
   require(ref.nonEmpty && alt.nonEmpty && ref.length != alt.length)
 }
