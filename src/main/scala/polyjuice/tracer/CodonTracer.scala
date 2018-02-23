30b71d3b511d0cd4303dc08893c73c1acc5b7a8c
Francisco Tanudjaja <fcstanud@gmail.com>
2018-02-22 22:20:18 -0800

    get amino acid

--- src/main/scala/polyjuice/tracer/CodonTracer.scala
+++ src/main/scala/polyjuice/tracer/CodonTracer.scala
@@ -1,25 +1,39 @@
 package polyjuice.tracer
 
+import scala.util.Try
+
 import polyjuice.model._
 
 case class CodonTracer(gene: Gene) {
   import CodonTracer._
 
-  def aminoAcid(codonNumber: Int, transcript: Transcript): Option[AminoAcid] = {
-    gene.get(transcript).flatMap(lookup(_, codonNumber))
+  def codon(pos: Int, transcript: Transcript): Option[Codon] = {
+    gene.get(transcript).flatMap(lookup(_, pos))
   }
 
-  def aminoAcid(codonNumber: Int): Map[Transcript, AminoAcid] = {
-    gene.flatMap {
-      case (transcript, g) => lookup(g, codonNumber).map((transcript, _))
-    }
+  def aminoAcid(pos: Int, transcript: Transcript): Option[AminoAcid] = {
+    codon(pos, transcript).flatMap(AminoAcid.ByCodon.get)
+  }
+
+  def codon(pos: Int): Map[Transcript, Codon] = {
+    for {
+      (transcipt, g) <- gene
+      codon <- lookup(g, pos)
+    } yield (transcipt, codon)
+  }
+
+  def aminoAcid(pos: Int): Map[Transcript, AminoAcid] = {
+    for {
+      (transcript, c) <- codon(pos)
+      aminoAcid <- AminoAcid.ByCodon.get(c)
+    } yield (transcript, aminoAcid)
   }
 }
 
 object CodonTracer {
 
-  def lookup(g: EnsemblGene, codonNumber: Int): Option[AminoAcid] = {
-    val codon = g.codingSequence.drop((codonNumber - 1) * 3).take(3)
-    AminoAcid.ByCodon.get(codon)
+  def lookup(g: EnsemblGene, codonNumber: Int): Option[Codon] = {
+    val bases = g.codingSequence.drop((codonNumber - 1) * 3).take(3)
+    if (bases.isEmpty) None else Try(Codon(bases)).toOption
   }
 }
