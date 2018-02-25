c72bde065d3234d13a5f1ae32dedd86e0b4e18d9
Francisco Tanudjaja <fcstanud@gmail.com>
2018-02-25 07:28:51 -0800

    nest base in companion object

--- src/main/scala/polyjuice/model/base.scala
+++ src/main/scala/polyjuice/model/base.scala
@@ -6,27 +6,27 @@ sealed trait Base {
   override def toString: String = s"$letter"
 }
 
-case object A extends Base {
-  override val letter: Char = 'A'
-  override val complement: Base = T
-}
-
-case object C extends Base {
-  override val letter: Char = 'C'
-  override val complement: Base = G
-}
-
-case object T extends Base {
-  override val letter: Char = 'T'
-  override val complement: Base = A
-}
-
-case object G extends Base {
-  override val letter: Char = 'G'
-  override val complement: Base = C
-}
-
 object Base {
+
+  case object A extends Base {
+    override val letter: Char = 'A'
+    override val complement: Base = T
+  }
+
+  case object C extends Base {
+    override val letter: Char = 'C'
+    override val complement: Base = G
+  }
+
+  case object T extends Base {
+    override val letter: Char = 'T'
+    override val complement: Base = A
+  }
+
+  case object G extends Base {
+    override val letter: Char = 'G'
+    override val complement: Base = C
+  }
 
   @throws[Exception]
   def apply(base: Char): Base = {
