adfb119ebba2ebd20657aea0e62eeb28cabac522
Francisco Tanudjaja <fcstanud@gmail.com>
2019-07-13 22:52:33 -0700

    update deploy scripts

---
+++ deploy/gce/install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+sudo apt-get -y install default-jdk
+
+chmod 755 start.sh 
--- deploy/gce/start.sh
+++ deploy/gce/start.sh
@@ -1,1 +1,3 @@
-nohup java -jar -Xmx256m -Dconfig.file=app.properties polyjuice-phial-assembly-0.1.0-SNAPSHOT.jar > app.log 2>&1 &
+#!/usr/bin/env bash
+
+nohup sudo java -jar -Xmx256m -Dconfig.file=app.properties polyjuice-phial-assembly-0.1.1-SNAPSHOT.jar > app.log 2>&1 &
