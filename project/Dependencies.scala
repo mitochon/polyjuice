fdf75dcf119aecc50a81b8b4fe737db6eca3af7f
Scala Steward <me@scala-steward.org>
2019-12-08 11:14:08 +0100

    Update http4s-blaze-server, http4s-circe, ... to 0.20.15

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   lazy val circeVersion = "0.12.1"
-  lazy val http4sVersion = "0.18.24"
+  lazy val http4sVersion = "0.20.15"
   lazy val logbackVersion = "1.2.3"
   lazy val scalaTestVersion = "3.0.8"
   lazy val typesafeConfigVersion = "1.4.0"
